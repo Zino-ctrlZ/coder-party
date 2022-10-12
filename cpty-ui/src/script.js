373c52345b28ae3b5618c2e609e1ddccf52cedec
Zino <epo1786@truman.edu>
2022-10-12 01:40:07 -0500

    utilizing js for image rendering

---
+++ cpty-ui/src/script.js
@@ -0,0 +1,11 @@
+var characters = document.getElementsByClassName('main--characters');
+var characterList = [
+	{ src: '../media/3d/girl-with-bird.pn ', id: 'character1' },
+	{ src: '../media/3d/black-girl-skipping.png', id: 'character2' },
+	{ src: '../media/3d/girl-sitting.png', id: 'character3' },
+	{ src: '../media/3d/man-hands.png', id: 'character4' },
+	{ src: '../media/3d/boy-laptop.png', id: 'character5' },
+	{ src: '../media/3d/boy-red-ball.png', id: 'character6' },
+	{ src: '../media/3d/man-in-shorts.png', id: 'character7' },
+	{ src: '../media/3d/girl-on-tree.png', id: 'character8' },
+];
