0eef64c8b60135e5535301cfd415cc43e0a6447a
Zino Onokpise <Zino.Onokpise@genesys.com>
2022-10-18 21:16:05 -0500

    wrapping entire content

--- cpty-ui/src/script.js
+++ cpty-ui/src/script.js
@@ -1,5 +1,5 @@
 var characterList = [
-	{ src: '../media/3d/girl-with-bird.pn ', id: 'character1' },
+	{ src: '../media/3d/girl-with-bird.png', id: 'character1' },
 	{ src: '../media/3d/black-girl-skipping.png', id: 'character2' },
 	{ src: '../media/3d/girl-sitting.png', id: 'character3' },
 	{ src: '../media/3d/man-hands.png', id: 'character4' },
@@ -9,17 +9,34 @@ var characterList = [
 	{ src: '../media/3d/girl-on-tree.png', id: 'character8' },
 ];
 
+console.log('script is running');
 function addCharacter() {
-	var characters = document.getElementsByClassName('main--characters');
-	const BREAKPOINT = 1152;
+	var characters = document.getElementById('main--characters');
+	const BREAKPOINT = 770;
 	if (window.innerWidth >= BREAKPOINT) {
-		for (let index = 0; index < characterList.length; index++) {
-			var character = document.createElement('img');
-			character.src = characterList[index].src;
-			character.setAttribute('id', `${characterList[index].id}`);
-			characters.appendChild(character);
+		console.log(window.innerWidth);
+		console.log(characters.children);
+		if (characters.children.length <= 0) {
+			for (let index = 0; index < characterList.length; index++) {
+				var character = document.createElement('img');
+				character.src = characterList[index].src;
+				character.id = `${characterList[index].id}`;
+				// character.setAttribute('id', );
+				characters.appendChild(character);
+			}
 		}
+	} else {
+		if (characters.children.length >= 0) {
+			while (characters.lastElementChild) {
+				characters.removeChild(characters.lastElementChild);
+			}
+		}
+		let randomIndex = Math.floor(Math.random() * characterList.length);
+		var character = document.createElement('img');
+		character.src = characterList[randomIndex].src;
+		character.id = `${characterList[randomIndex].id}`;
+		characters.appendChild(character);
 	}
 }
 
-window.addEventListener('resize', addCharacter);
+window.addEventListener('DOMContentLoaded', addCharacter);
