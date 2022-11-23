ba948a3aa6746afb7d62278578447e7740e0e0f3
Zino <epo1786@truman.edu>
2022-11-22 22:21:10 -0600

    improved css on nav

--- cpty-ui/src/script.js
+++ cpty-ui/src/script.js
@@ -1,5 +1,5 @@
 var characterList = [
-	{ src: '../media/3d/girl-with-bird.pn ', id: 'character1' },
+	{ src: '../media/3d/girl-with-bird.png ', id: 'character1' },
 	{ src: '../media/3d/black-girl-skipping.png', id: 'character2' },
 	{ src: '../media/3d/girl-sitting.png', id: 'character3' },
 	{ src: '../media/3d/man-hands.png', id: 'character4' },
@@ -9,16 +9,30 @@ var characterList = [
 	{ src: '../media/3d/girl-on-tree.png', id: 'character8' },
 ];
 
+console.log('script running');
 function addCharacter() {
-	var characters = document.getElementsByClassName('main--characters');
-	const BREAKPOINT = 1152;
+	var characters = document.getElementById('main--characters');
+	const BREAKPOINT = 770;
 	if (window.innerWidth >= BREAKPOINT) {
-		for (let index = 0; index < characterList.length; index++) {
-			var character = document.createElement('img');
-			character.src = characterList[index].src;
-			character.setAttribute('id', `${characterList[index].id}`);
-			characters.appendChild(character);
+		if (characters.children.length <= 0) {
+			for (let index = 0; index < characterList.length; index++) {
+				var character = document.createElement('img');
+				character.src = characterList[index].src;
+				character.id = characterList[index].id;
+				characters.appendChild(character);
+			}
 		}
+	} else {
+		if (characters.children.length >= 0) {
+			while (characters.lastElementChild) {
+				characters.removeChild(characters.lastElementChild);
+			}
+		}
+		let randomIndex = Math.floor(Math.random() * characterList.length);
+		var character = document.createElement('img');
+		character.src = characterList[randomIndex].src;
+		character.id = characterList[randomIndex].id;
+		characters.appendChild(character);
 	}
 }
 
