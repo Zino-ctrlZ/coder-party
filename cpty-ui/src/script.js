6cf3226ff1a43ee9ae39997e41b113dcf9bd0555
Zino <epo1786@truman.edu>
2022-12-01 00:26:19 -0600

    added footer image

--- cpty-ui/src/script.js
+++ cpty-ui/src/script.js
@@ -1,28 +1,21 @@
 var characterList = [
-	{ src: '../media/3d/girl-with-bird.png ', id: 'character1' },
-	{ src: '../media/3d/black-girl-skipping.png', id: 'character2' },
-	{ src: '../media/3d/girl-sitting.png', id: 'character3' },
-	{ src: '../media/3d/man-hands.png', id: 'character4' },
-	{ src: '../media/3d/boy-laptop.png', id: 'character5' },
-	{ src: '../media/3d/boy-red-ball.png', id: 'character6' },
-	{ src: '../media/3d/man-in-shorts.png', id: 'character7' },
-	{ src: '../media/3d/girl-on-tree.png', id: 'character8' },
+	{ src: '../media/3d/girl-with-bird.png ', id: 'character' },
+	{ src: '../media/3d/black-girl-skipping.png', id: 'character' },
+	{ src: '../media/3d/girl-sitting.png', id: 'character' },
+	{ src: '../media/3d/man-hands.png', id: 'character' },
+	{ src: '../media/3d/boy-laptop.png', id: 'character' },
+	{ src: '../media/3d/boy-red-ball.png', id: 'character' },
+	{ src: '../media/3d/man-in-shorts.png', id: 'character' },
+	{ src: '../media/3d/girl-on-tree.png', id: 'character' },
 ];
+
+var mainFooterImg = { src: '../media/3d/footer.png', id: 'footer-img' };
 
 console.log('script running');
 function addCharacter() {
 	var characters = document.getElementById('main--characters');
 	const BREAKPOINT = 770;
-	if (window.innerWidth >= BREAKPOINT) {
-		if (characters.children.length <= 0) {
-			for (let index = 0; index < characterList.length; index++) {
-				var character = document.createElement('img');
-				character.src = characterList[index].src;
-				character.id = characterList[index].id;
-				characters.appendChild(character);
-			}
-		}
-	} else {
+	if (window.innerWidth <= BREAKPOINT) {
 		if (characters.children.length >= 0) {
 			while (characters.lastElementChild) {
 				characters.removeChild(characters.lastElementChild);
@@ -36,4 +29,4 @@ function addCharacter() {
 	}
 }
 
-window.addEventListener('resize', addCharacter);
+window.addEventListener('DOMContentLoaded', addCharacter);
